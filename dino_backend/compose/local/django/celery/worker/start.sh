7c8108d429febf34a3ea74dd348733297177242a
AlexTeusz <alexander.teusz@hhu.de>
2018-05-15 10:22:32 +0200

    deleted cookiecutter project

--- dino_backend/compose/local/django/celery/worker/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -o errexit
-set -o pipefail
-set -o nounset
-set -o xtrace
-
-
-celery -A dino_backend.taskapp worker -l INFO
--- dino_backend/compose/production/django/celery/worker/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -o errexit
-set -o pipefail
-set -o nounset
-
-
-celery -A dino_backend.taskapp worker -l INFO
