c0177acdc514ff0500d69872f39a43edbb571165
AlexTeusz <alexander.teusz@hhu.de>
2018-06-22 18:49:31 +0200

    changed AWS data because of globals import

--- dino/database/dynamoDB/get_table_content.py
+++ dino/database/dynamoDB/get_table_content.py
@@ -1,8 +1,10 @@
 import boto3
+import globals
+
 dynamodb = boto3.resource('dynamodb',
-    aws_access_key_id = '',
-    aws_secret_access_key = '',
-    region_name = 'eu-west-1'
+    aws_access_key_id = globals.AWS_ACCESS_KEY,
+    aws_secret_access_key = globals.AWS_SECRET_ACCESS_KEY,
+    region_name = globals.AWS_REGION
     )
 
 table = dynamodb.Table('Routes')
--- dino/database/dynamoDB/put_table_content.py
+++ dino/database/dynamoDB/put_table_content.py
@@ -1,12 +1,13 @@
 import boto3
 from generateID import ID
+import globals
 
 random_id = ID.generate_id()
 
 dynamodb = boto3.resource('dynamodb',
-    aws_access_key_id = '',
-    aws_secret_access_key = 'hV+',
-    region_name = 'eu-west-1'
+    aws_access_key_id = globals.AWS_ACCESS_KEY,
+    aws_secret_access_key = globals.AWS_SECRET_ACCESS_KEY,
+    region_name = globals.AWS_REGION
     )
 
 table = dynamodb.Table('Routes')
